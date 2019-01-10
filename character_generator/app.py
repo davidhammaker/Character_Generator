5d4e42454cf029060c9e691aa8fc5af4f4b09279
David J. Hammaker <davidjosh93@gmail.com>
2019-01-09 22:08:04 -0500

    Add backgrounds and subraces to generator
    The character generator now applies a random background and, if 
    applicable, subrace to each generated character.

--- character_generator/app.py
+++ character_generator/app.py
@@ -11,6 +11,14 @@ races = ['Dwarf',
          'Half-Orc',
          'Tiefling']
 
+subraces = [['Dwarf', ['Hill Dwarf', 'Mountain Dwarf']],
+            ['Elf', [['High Elf', ['Sun Elf', 'Moon Elf']],
+                     'Wood Elf',
+                     'Dark Elf (Drow)']],
+            ['Halfling', ['Lightfoot', 'Stout']],
+            ['Human', ['Standard', 'Variant']],
+            ['Gnome', ['Forest Gnome', 'Rock Gnome']]]
+
 classes = ['Barbarian',
            'Bard',
            'Cleric',
@@ -41,16 +49,32 @@ alignments_modded = [['Lawful', 'Good'],
                      ['Chaotic', 'Good'],
                      ['Chaotic', 'Neutral']]
 
+backgrounds = ['Acolyte',
+               'Charlatan',
+               'Criminal',
+               'Entertainer',
+               'Folk Hero',
+               'Guild Artisan',
+               'Hermit',
+               'Noble',
+               'Outlander',
+               'Sage',
+               'Sailor',
+               'Soldier',
+               'Urchin']
+
 
 class Character:
     """A class defining generated characters. Use the
     'character_generator' method to randomly generate values for a
     character."""
 
-    def __init__(self, race, alignment, cls):
+    def __init__(self, race, alignment, cls, background, subrace=None):
         self._race = race
+        self._subrace = subrace
         self._alignment = alignment
         self._cls = cls
+        self._background = background
 
     def __repr__(self):
         return f"<Character '{self._race}', '{self._alignment[0]} " \
@@ -60,6 +84,9 @@ class Character:
         """Return race as a string."""
         return self._race
 
+    def subrace(self):
+        return self._subrace
+
     def alignment(self):
         """Return alignment as a list."""
         return self._alignment
@@ -67,6 +94,10 @@ class Character:
     def cls(self):
         """Return class as a string."""
         return self._cls
+
+    def background(self):
+        """Return background as a string."""
+        return self._background
 
 
 def character_generator(evil_permitted=False):
@@ -89,9 +120,21 @@ def character_generator(evil_permitted=False):
 
     race = races[int(random() * len(races))]
 
+    # Select a subrace
+    subrace = None
+    for selection in subraces:
+        if race in selection:
+            subrace = selection[1][int(random() * len(selection[1]))]
+    # Specify type of High Elf
+    if subrace:
+        if 'High Elf' in subrace:
+            subrace = f'High Elf ({subrace[1][int(random() * 2)]})'
+
     cls = classes[int(random() * len(classes))]
 
-    c = Character(race=race, alignment=alignment, cls=cls)
+    background = backgrounds[int(random() * len(backgrounds))]
+
+    c = Character(race=race, alignment=alignment, cls=cls, background=background, subrace=subrace)
 
     return c
 
@@ -105,5 +148,6 @@ if __name__ == '__main__':
     c = character_generator(evil_permitted=evil_permitted)
 
     print(f'''Race: {c.race()}
+Subrace: {c.subrace()}
 Alignment: {c.alignment()[0]} {c.alignment()[1]}
 Class: {c.cls()}''')
