0e44855822bc3de2533dcd052ad16a9c1a6e585c
David J. Hammaker <davidjosh93@gmail.com>
2019-01-09 23:39:25 -0500

    Add archetypes
    Add archetypes to character generation. By default, a Character 
    archetype is set to None. In the future, the archetype will remain None 
    until the character is at a level high enough to attain an archetype. 
    For now, however, the generator always creates an archetype, overwriting 
    the None value.

--- character_generator/app.py
+++ character_generator/app.py
@@ -32,6 +32,65 @@ classes = ['Barbarian',
            'Warlock',
            'Wizard']
 
+archetypes = [['Barbarian', ['Path of the Berserker',
+                             'Path of the Totem Warrior']],
+              ['Bard', ['College of Lore',
+                        'College of Valor']],
+              ['Cleric', ['Life Domain',
+                          'Light Domain',
+                          'Nature Domain',
+                          'Tempest Domain',
+                          'Trickery Domain',
+                          'War Domain']],
+              ['Druid', [['Circle of the Land', ['Arctic',
+                                                 'Coast',
+                                                 'Desert',
+                                                 'Forest',
+                                                 'Grassland',
+                                                 'Mountain',
+                                                 'Swamp',
+                                                 'Underdark']],
+                         'Circle of the Moon']],
+              ['Fighter', ['Champion',
+                           'Battle Master',
+                           'Eldritch Knight']],
+              ['Monk', ['Way of the Open Hand',
+                        'Way of Shadow',
+                        'Way of the Four Elements']],
+              ['Paladin', ['Oauth of Devotion',
+                           'Oauth of the Ancients',
+                           'Oath of Vengeance']],
+              ['Ranger', ['Hunter',
+                          'Beast Master']],
+              ['Rogue', ['Thief',
+                         'Assassin',
+                         'Archane Trixter']],
+              ['Sorcerer', [['Draconic Bloodline', ['Black',
+                                                    'Blue',
+                                                    'Brass',
+                                                    'Bronze',
+                                                    'Copper',
+                                                    'Gold',
+                                                    'Green',
+                                                    'Red',
+                                                    'Silver',
+                                                    'White']],
+                            'Wild Magic']],
+              ['Warlock', [['The Archfey',
+                            'The Fiend',
+                            'The Great Old One'],
+                           ['Pact of the Chain',
+                            'Pact of the Blade',
+                            'Pact of the Tome']]],
+              ['Wizard', ['School of Abjuration',
+                          'School of Conjuration',
+                          'School of Divination',
+                          'School of Enchantment',
+                          'School of Evocation',
+                          'School of Illusion',
+                          'School of Necromancy',
+                          'School of Transmutation']]]
+
 alignments = [['Lawful', 'Good'],
               ['Lawful', 'Neutral'],
               ['Lawful', 'Evil'],
@@ -69,12 +128,14 @@ class Character:
     'character_generator' method to randomly generate values for a
     character."""
 
-    def __init__(self, race, alignment, cls, background, subrace=None):
+    def __init__(self, race, alignment, cls, background, subrace=None,
+                 archetype=None):
         self._race = race
         self._subrace = subrace
         self._alignment = alignment
         self._cls = cls
         self._background = background
+        self._archetype = archetype
 
     def __repr__(self):
         return f"<Character '{self._race}', '{self._alignment[0]} " \
@@ -94,6 +155,10 @@ class Character:
     def cls(self):
         """Return class as a string."""
         return self._cls
+
+    def archetype(self):
+        """Return archetype as a string."""
+        return self._archetype
 
     def background(self):
         """Return background as a string."""
@@ -128,13 +193,39 @@ def character_generator(evil_permitted=False):
     # Specify type of High Elf
     if subrace:
         if 'High Elf' in subrace:
-            subrace = f'High Elf ({subrace[1][int(random() * 2)]})'
+            subrace = f'High Elf ' \
+                f'({subrace[1][int(random() * len(subrace[1]))]})'
 
     cls = classes[int(random() * len(classes))]
 
+    # Select an archetype
+    archetype = None
+    for selection in archetypes:
+        if cls != 'Warlock' and cls in selection:
+            archetype = selection[1][int(random() * len(selection[1]))]
+        # Warlock Archetype selection (Patron and Pact)
+        elif cls == 'Warlock' and cls in selection:
+            patrons = selection[1][0]
+            pacts = selection[1][1]
+            archetype = f'{patrons[int(random() * len(patrons))]} ' \
+                f'({pacts[int(random() * len(pacts))]})'
+    # Specify Druid's Land
+    if 'Circle of the Land' in archetype:
+        archetype = f'Circle of the Land ' \
+            f'({archetype[1][int(random() * len(archetype[1]))]})'
+    # Specify Sorcerer's Dragon
+    if 'Draconic Bloodline' in archetype:
+        archetype = f'Draconic Bloodline ' \
+            f'({archetype[1][int(random() * len(archetype[1]))]})'
+
     background = backgrounds[int(random() * len(backgrounds))]
 
-    c = Character(race=race, alignment=alignment, cls=cls, background=background, subrace=subrace)
+    c = Character(race=race,
+                  alignment=alignment,
+                  cls=cls,
+                  background=background,
+                  subrace=subrace,
+                  archetype=archetype)
 
     return c
 
