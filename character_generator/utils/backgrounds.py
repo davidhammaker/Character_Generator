e036c8e76f7f42e1eb7752390f45c6d787bd3565
David J. Hammaker <davidjosh93@gmail.com>
2019-01-24 15:44:22 -0500

    Add 'criminal' Background instance

--- character_generator/utils/backgrounds.py
+++ character_generator/utils/backgrounds.py
@@ -142,3 +142,82 @@ charlatan = \
                bonds=_charlatan_bonds,
                flaws=_charlatan_flaws)
 
+# Criminal
+# Criminal skill proficiencies
+_criminal_skill_profs = \
+    ['Deception',
+     'Stealth']
+
+# Criminal tool proficiencies
+_criminal_tool_profs = \
+    ['thieves\' tools']
+_criminal_tool_prof_selection = \
+    ['dice set', 'dragonchess set',
+     'playing card set',
+     'Three-Dragon Ante set']
+
+# Criminal equipment
+_criminal_equipment = ['crowbar', 'dark common clothes with hood']
+
+# Criminal selection
+_criminal_selection = \
+    ['Criminal Specialty: Blackmailer',
+     'Criminal Specialty: Burglar',
+     'Criminal Specialty: Enforcer',
+     'Criminal Specialty: Fence',
+     'Criminal Specialty: Highway robber',
+     'Criminal Specialty: Hired killer',
+     'Criminal Specialty: Pickpocket',
+     'Criminal Specialty: Smuggler']
+
+# Criminal details
+_criminal_details = 'Feature: Criminal Contact'
+
+# Criminal characteristics
+_criminal_personalities = \
+    ['I always have a plan for what to do when things go wrong.',
+     'I am always calm, no matter what the situation. I never raise my voice or let my emotions control me.',
+     'The first thing I do in a new place is note the locations of everything valuable -- or where such things could be hidden.',
+     'I would rather make a new friend than a new enemy.',
+     'I am incredibly slow to trust. Those who seem the fairest often have the most to hide.',
+     'I don\'t pay attention to the risks in a situation. Never tell me the odds.',
+     'The best way to get me to do something is to tell me I can\'t do it.',
+     'I blow up at the slightest insult.']
+_criminal_ideals = \
+    ['Honor. I don\'t steal from others in the trade. (Lawful)',
+     'Freedom. Chains are meant to be broken, as are those who would forge them. (Chaotic)',
+     'Charity. I steal from the wealthy so that I can help people in need. (Good)',
+     'Greed. I will do whatever it takes to become wealthy. (Evil)',
+     'People. I\'m loyal to my friends, not to any ideals, and everyone else can take a trip down the Styx for all I care. (Neutral)',
+     'Redemption. There\'s a spark of good in everyone. (Good)']
+_criminal_bonds = \
+    ['I\'m trying to pay off an old debt I owe to a generous benefactor.',
+     'My ill-gotten gains go to support my family.',
+     'Something important was taken from me, and I aim to steal it back.',
+     'I will become the greatest thief that ever lived.',
+     'I\'m guilty of a terrible crime. I hope I can redeem myself for it.',
+     'Someone I loved died because of a mistake I made. That will never happen again.']
+_criminal_flaws = \
+    ['When I see something valuable, I can\'t think about anything but how to steal it.',
+     'When faced with a choice between money and my friends, I usually choose the money.',
+     'If there\'s a plan, I\'ll forget it. If I don\'t forget it, I\'ll ignore it.',
+     'I have a "tell" that reveals when I\'m lying.',
+     'I turn tail and run when things look bad.',
+     'An innocent person is in prison for a crime that I committed. I\'m okay with that.']
+
+# Criminal Background
+criminal = \
+    Background(name='Criminal',
+               skill_profs=_criminal_skill_profs,
+               tool_profs=_criminal_tool_profs,
+               tool_prof_selection=_criminal_tool_prof_selection,
+               equipment=_criminal_equipment,
+               equipment_selections=None,
+               selections=_criminal_selection,
+               money=Money(gp=15),
+               languages=0,
+               details=_criminal_details,
+               personalities=_criminal_personalities,
+               ideals=_criminal_ideals,
+               bonds=_criminal_bonds,
+               flaws=_criminal_flaws)
